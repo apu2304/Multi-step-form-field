d9a1b3946ca96023fbe1aadee18210dbf215ee5a
unknown <paulsarpita@gmail.com>
2025-01-06 18:12:20 +0530

    Update Vite main component

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <Router basename="/Multi-step-form-field">
     <App />
     </Router>
   </StrictMode>,
